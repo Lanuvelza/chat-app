c200d8598c9bd5e0bc756ce78ece1f9f620824bc
Ian <ianlam1221@hotmail.com>
2021-04-11 01:05:55 -0700

    added socket handler

--- client/app.js
+++ client/app.js
@@ -1,11 +1,19 @@
-alert('hello from the JS file');
+// alert('hello from the JS file');
+
+const socket = io();
 
 $(document).ready(function() {
 
   $('form').on('submit', function(event) {
     event.preventDefault();
-    var text = $('#message').val(); 
-    alert(text);
+    const text = $('#message').val(); 
+    socket.emit('message', text);
+    $('#message').val('');
     return false;
-  })
+  });
+
+  socket.on('message', function(msg) {
+    $('<li>').text(msg).appendTo('#history');
+  });
+
 })
